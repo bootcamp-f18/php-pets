9406d684eb6b22ec1549e8c7e427cd0b436b63bc
Philip Devine <devine.pk@gmail.com>
2018-10-24 16:58:34 -0400

    adds new page to add a pet, and has some fields completed.  No php yet.

--- addpet.php
+++ addpet.php
@@ -8,9 +8,11 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>PHP Pets Adoption Service</title>
+    <title>Put Your Pet Up For Adoption</title>
   </head>
   <body class="container mt-3">
+
+
 
 
 <?php
@@ -25,11 +27,51 @@ ORDER BY name");
 
 ?>
 
-    <h1>PHP Pets Adoption Service</h1>
+    <h1>Put Your Pet Up For Adoption</h1>
 
-    <h3 class="mb-4 mt-5"><a href="addpet.php">Add Your Pet</a></h3>
 
-    <h3 class="mb-4 mt-4">List of Pets</h3>
+    <form>
+      <div class="form-group">
+        <label for="petName">Pet Name</label>
+        <input type="text" class="form-control" id="petName">
+      </div>
+      <div class="form-group">
+        <label for="species">Species</label>
+        <input type="text" class="form-control" id="species">
+      </div>
+      <div class="form-group">
+        <label for="breed">Breed</label>
+        <input type="text" class="form-control" id="breed">
+      </div>
+      <div class="form-group">
+        <label for="description">Description</label>
+        <input type="text" class="form-control" id="description">
+      </div>
+      <div class="form-group">
+        <label for="age_in_years">Age (in Years)</label>
+        <input type="number" class="form-control" id="ageInYears">
+      </div>
+      <div class="form-group">
+        <label for="weight_in_pounds">Weight In Pounds</label>
+        <input type="number" class="form-control" id="weightInPounds">
+      </div>
+      <div class="form-group">
+        <label for="care_description">Description of Care</label>
+        <input type="text" class="form-control" id="careDescription">
+      </div>
+      <p>Is Your Pet Good With Kids?</p>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input type="radio" id="isBadWithKids1" name="isBadWithKids1" class="custom-control-input">
+        <label class="custom-control-label" for="isBadWithKids1">No</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input type="radio" id="isGoodWithKids2" name="isGoodWithKids2" class="custom-control-input">
+        <label class="custom-control-label" for="isGoodWithKids2">Yes</label>
+      </div>
+
+
+      <!-- ^^ toggle isn't working -->
+<!--    <h3 class="mb-4 mt-4">List of Pets</h3>
 
     <table class="table">
         <thead>
@@ -56,7 +98,7 @@ ORDER BY name");
 ?>
 
         </tbody>
-    </table>
+    </table> -->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
